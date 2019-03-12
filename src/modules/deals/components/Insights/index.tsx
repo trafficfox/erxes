6c401507ebcd972e08945c87d5fee47f51a7614b
Dulguun Otgon <dulguun0225@gmail.com>
2019-03-12 19:10:32 +0800

    ui

--- src/modules/deals/components/Insights/index.tsx
+++ src/modules/deals/components/Insights/index.tsx
@@ -1,3 +1,5 @@
+import { Wrapper } from 'modules/layout/components';
+import { SidebarList } from 'modules/layout/styles';
 import * as React from 'react';
 import { NavLink, Route } from 'react-router-dom';
 import Lost from './Lost';
@@ -9,17 +11,32 @@ class Insights extends React.Component<{ match: any }> {
   render() {
     const { match } = this.props;
     return (
-      <>
-        <NavLink to={`${match.url}/volume`}>Deals volume report</NavLink>
-        <NavLink to={`${match.url}/started`}>Deals started report</NavLink>
-        <NavLink to={`${match.url}/won`}>Deals won report</NavLink>
-        <NavLink to={`${match.url}/lost`}>Deals lost report</NavLink>
-
-        <Route path={`${match.path}/volume`} component={Volume} />
-        <Route path={`${match.path}/started`} component={Started} />
-        <Route path={`${match.path}/won`} component={Won} />
-        <Route path={`${match.path}/lost`} component={Lost} />
-      </>
+      <Wrapper
+        leftSidebar={
+          <Wrapper.Sidebar>
+            <Wrapper.Sidebar.Section>
+              <SidebarList>
+                <NavLink to={`${match.url}/volume`}>
+                  Deals volume report
+                </NavLink>
+                <NavLink to={`${match.url}/started`}>
+                  Deals started report
+                </NavLink>
+                <NavLink to={`${match.url}/won`}>Deals won report</NavLink>
+                <NavLink to={`${match.url}/lost`}>Deals lost report</NavLink>
+              </SidebarList>
+            </Wrapper.Sidebar.Section>
+          </Wrapper.Sidebar>
+        }
+        content={
+          <>
+            <Route path={`${match.path}/volume`} component={Volume} />
+            <Route path={`${match.path}/started`} component={Started} />
+            <Route path={`${match.path}/won`} component={Won} />
+            <Route path={`${match.path}/lost`} component={Lost} />
+          </>
+        }
+      />
     );
   }
 }
